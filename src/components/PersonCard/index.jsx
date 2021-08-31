d0a8fbcaaa918be9e5caf8491c0a5a411425f3aa
Smilly <smilly.3d@gmail.com>
2021-08-31 18:35:22 -0300

    fixed

--- src/components/PersonCard/index.jsx
+++ src/components/PersonCard/index.jsx
@@ -1,14 +1,34 @@
 import "./styles.css";
-function PersonCard({ name, age, country }) {
-  return (
-    <div className="UserCard">
-      <span>
-        <p>name: {name}</p>
-        <p>age: {age}</p>
-        <p>country: {country}</p>
-      </span>
-    </div>
-  );
+const person = [
+  {
+    name: "Joan",
+    age: 20,
+    country: "PA",
+  },
+  {
+    name: "Joane",
+    age: 21,
+    country: "SC",
+  },
+  {
+    name: "James",
+    age: 30,
+    country: "MA",
+  },
+];
+
+function PersonCard() {
+  const card = person.map((person, index) => {
+    return (
+      <div className="UserCard" key={index}>
+        <div>Name: {person.name}</div>
+        <div>Age: {person.age}</div>
+        <div>Country: {person.country}</div>
+      </div>
+    );
+  });
+
+  return <div>{card}</div>;
 }
 
 export default PersonCard;
